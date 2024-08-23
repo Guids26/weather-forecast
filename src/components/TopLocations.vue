ea84d3b844ffed0da0554772e1f72118f3e35c14
Guids26 <guilhermedantas006@gmail.com>
2024-08-23 12:00:56 -0300

    feat adding reactivity from top locations

--- src/components/TopLocations.vue
+++ src/components/TopLocations.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="q-ma-md bg-orange">
         <q-table :title="$t('topLocations')" :rows-per-page-options="[0]" hide-bottom :loading="loadingInfos"
-            :rows="locationStore.topLocations" :columns="columns" row-key="name" />
+            :rows="topLocations" :columns="columns" row-key="name" />
     </div>
 </template>
 
@@ -23,27 +23,28 @@ const weatherData = reactive({});
 const loadingInfos = ref(weatherApi.loading);
 
 const configStore = useConfigStore();
-const { temperatureUnit } = storeToRefs(configStore)
+const { temperatureUnit } = storeToRefs(configStore);
 
+const topLocations = computed(() => locationStore.getTopLocations);
 
 const convertTemperature = (temp, unit) => {
     return unit === 'fahrenheit' ? (temp * (9 / 5)) + 32 : (5 / 9) * (temp - 32);
 }
 
 const getWeatherInfos = async (location) => {
-    if (!weatherData[location.name]) {
+    if (!weatherData[location.latLong]) {
         loadingInfos.value = true;
         const latitude = location.latLong.split(",")[0]
         const longitude = location.latLong.split(",")[1]
         const data = await weatherApi.searchCurrentMinAndMaxTemperature(latitude, longitude);
-        weatherData[location.name] = {
+        weatherData[location.latLong] = {
             min: data.minTemperature,
             max: data.maxTemperature,
         };
         loadingInfos.value = false;
     } else {
-        weatherData[location.name].min = convertTemperature(weatherData[location.name].min, temperatureUnit.value);
-        weatherData[location.name].max = convertTemperature(weatherData[location.name].max, temperatureUnit.value);
+        weatherData[location.latLong].min = convertTemperature(weatherData[location.latLong].min, temperatureUnit.value);
+        weatherData[location.latLong].max = convertTemperature(weatherData[location.latLong].max, temperatureUnit.value);
     }
 }
 
@@ -62,7 +63,7 @@ const columns = computed(() => [
         required: true,
         label: t('min'),
         align: 'center',
-        field: row => weatherData[row.name]?.min || t('loading'),
+        field: row => weatherData[row.latLong]?.min || t('loading'),
         format: val => Math.round(val) + " " + (temperatureUnit.value == "celsius" ? "ºC" : "ºF"),
         sortable: true
     },
@@ -71,7 +72,7 @@ const columns = computed(() => [
         required: true,
         label: t('max'),
         align: 'center',
-        field: row => weatherData[row.name]?.max || t('loading'),
+        field: row => weatherData[row.latLong]?.max || t('loading'),
         format: val => Math.round(val) + " " + (temperatureUnit.value == "celsius" ? "ºC" : "ºF"),
         sortable: true
     }
